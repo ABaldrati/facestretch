f9747e1568517e64157074a59698f28c38f5eba0
Giovanni Berti <dev.giovanniberti@gmail.com>
2020-07-20 18:00:57 +0200

    refactor(detect_landmarks): add `main` function

--- detect_landmarks.py
+++ detect_landmarks.py
@@ -45,94 +45,92 @@ def shape_to_np(shape, dtype="int"):
     return coords
 
 
-# Initialize the camera
-cv2.namedWindow("FaceLandmarks")
-cap = cv2.VideoCapture(0)
-# vc.set(3, 320)
-# vc.set(4, 240)
+def main():
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-if cap.isOpened():
-    rval, frame = cap.read()
-else:
-    rval = False
+    # Initialize the camera
+    cv2.namedWindow("FaceLandmarks")
+    cap = cv2.VideoCapture(0)
+    # vc.set(3, 320)
+    # vc.set(4, 240)
 
-# construct the argument parser and parse the arguments
-##ap = argparse.ArgumentParser()
-##ap.add_argument("-p", "--shape-predictor", required=True, help="path to facial landmark predictor")
-##ap.add_argument("-i", "--image", required=True, help="path to input image")
-##args = vars(ap.parse_args())
-# initialize dlib's face detector (HOG-based) and then create
-# the facial landmark predictor
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
-print(type(detector))
+    if cap.isOpened():
+        rval, frame = cap.read()
+    else:
+        rval = False
 
-grab_next_landmark_frame = False
-while rval:
-    # Read frame and convert to grayscale
-    rval, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # detect faces in the grayscale image
-    rects = detector(gray, 0)
-    # loop over the face detections
-    for (i, rect) in enumerate(rects):
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y)-coordinates to a NumPy
-        # array
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
+    grab_next_landmark_frame = False
+    while rval:
+        # Read frame and convert to grayscale
+        rval, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # convert dlib's rectangle to a OpenCV-style bounding box
-        # [i.e., (x, y, w, h)], then draw the face bounding box
-        (x, y, w, h) = face_utils.rect_to_bb(rect)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # detect faces in the grayscale image
+        rects = detector(gray, 0)
+        # loop over the face detections
+        for (i, rect) in enumerate(rects):
+            # determine the facial landmarks for the face region, then
+            # convert the facial landmark (x, y)-coordinates to a NumPy
+            # array
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
 
-        # show the face number
-        # cv2.putText(frame, "Face #{}".format(i + 1), (x - 10, y - 10),
-        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            # convert dlib's rectangle to a OpenCV-style bounding box
+            # [i.e., (x, y, w, h)], then draw the face bounding box
+            (x, y, w, h) = face_utils.rect_to_bb(rect)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        # loop over the (x, y)-coordinates for the facial landmarks
-        # and draw them on the image
-        key = cv2.waitKey(1)
+            # show the face number
+            # cv2.putText(frame, "Face #{}".format(i + 1), (x - 10, y - 10),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        if key == ord('c'):
-            for (x, y) in shape:
-                cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+            # loop over the (x, y)-coordinates for the facial landmarks
+            # and draw them on the image
+            key = cv2.waitKey(1)
 
-        rect = cv2.minAreaRect(shape)
-        ((x, y), _, angle) = rect
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
+            if key == ord('c'):
+                for (x, y) in shape:
+                    cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
 
-        cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
+            rect = cv2.minAreaRect(shape)
+            ((x, y), _, angle) = rect
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
 
-        if key == ord('q'):
-            rval = False
+            cv2.drawContours(frame, [box], 0, (0, 0, 255), 2)
 
-        if key == ord("s"):
-            grab_next_landmark_frame = True
+            if key == ord('q'):
+                rval = False
 
-        if grab_next_landmark_frame:
-            reference_landmark = extract_landmarks(frame)
-            if reference_landmark != []:
-                norm_ref_landmark = normalize_landmarks(reference_landmark[0]).flatten()
-                grab_next_landmark_frame = False
-                print("Successfully saved reference neuter image")
+            if key == ord("s"):
+                grab_next_landmark_frame = True
 
-        if norm_ref_landmark is not None:
-            normalized_landmarks = normalize_landmarks(shape).flatten() - norm_ref_landmark
+            if grab_next_landmark_frame:
+                reference_landmark = extract_landmarks(frame)
+                if reference_landmark != []:
+                    norm_ref_landmark = normalize_landmarks(reference_landmark[0]).flatten()
+                    grab_next_landmark_frame = False
+                    print("Successfully saved reference neuter image")
 
-            distance_func = model.get_metric()
-            distance = distance_func(action_reference_landmarks, normalized_landmarks)
-            yes_no = model.predict([[action_reference_landmarks, normalized_landmarks]])
-            cv2.putText(frame, f"distance {distance:.2f} {yes_no}", (int(x) - 10, int(y) - 100),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 0), 2)
+            if norm_ref_landmark is not None:
+                normalized_landmarks = normalize_landmarks(shape).flatten() - norm_ref_landmark
 
-            # for (x, y) in normalized_landmarks:
-            #     cv2.circle(frame, (int(x * 250 + 150), int(y * 250 + 150)), 1, (0, 255, 255), -1)
+                distance_func = model.get_metric()
+                distance = distance_func(action_reference_landmarks, normalized_landmarks)
+                yes_no = model.predict([[action_reference_landmarks, normalized_landmarks]])
+                cv2.putText(frame, f"distance {distance:.2f} {yes_no}", (int(x) - 10, int(y) - 100),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 0), 2)
 
-    cv2.imshow("FaceLandmarks", frame)
+                # for (x, y) in normalized_landmarks:
+                #     cv2.circle(frame, (int(x * 250 + 150), int(y * 250 + 150)), 1, (0, 255, 255), -1)
 
-cap.release()
-cv2.destroyAllWindows()
+        cv2.imshow("FaceLandmarks", frame)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
