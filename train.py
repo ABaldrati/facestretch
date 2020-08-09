c73f03ec1bc986577efbe6ddacfceab1eee7821e
ABaldrati <a_baldrati@hotmail.it>
2020-08-09 17:42:43 +0200

    feat(train): select which model you want to train

--- train.py
+++ train.py
@@ -3,49 +3,68 @@ from pathlib import Path
 
 import numpy as np
 from joblib import dump
-from metric_learn import ITML
+from metric_learn import ITML, LMNN, NCA, LFDA, SDML, MMC
 
-from dataset import generate_weakly_supervised_interpolated_dataset
+from dataset import generate_weakly_supervised_interpolated_dataset, generate_training_supervised_dataset_categorical
 from detect_landmarks import MODELS_PATH
 
 
 def main():
-    # generate_video_dataset(Path("dataset_video").resolve())
+    NAME_MODEL_TO_TRAIN = "NCA"
+    # weakly-supervised possibilities "ITML" , "SDML" , "MMC"
+    # supervised possibilities: "LMNN" , "NCA" , "LFDA"
 
-    # landmarks_matrix, training_pairs_labels = generate_training_supervised_dataset_regression(
-    #    Path("dataset_video_images").resolve())
+    rates_list = [0.5, 1]
 
-    saved_landmarks_matrix = Path("landmarks_matrix.npy")
-    saved_training_pairs_indices = Path("training_pairs_indices.npy")
-    saved_training_pairs_labels = Path("training_pairs_labels.npy")
-
-    if not saved_landmarks_matrix.exists() or not saved_training_pairs_indices.exists() or not saved_training_pairs_labels.exists():
-        print("Generating dataset...")
-
-        landmarks_matrix, training_pairs_indices, training_pairs_labels = generate_weakly_supervised_interpolated_dataset(
-            Path("dataset_new").resolve(), [0.5, 1])
-
-        np.save(str(saved_landmarks_matrix), landmarks_matrix)
-        np.save(str(saved_training_pairs_indices), training_pairs_indices)
-        np.save(str(saved_training_pairs_labels), training_pairs_labels)
-    else:
-        print("Loading dataset from filesystem...")
-        landmarks_matrix = np.load(saved_landmarks_matrix)
-        training_pairs_indices = np.load(saved_training_pairs_indices)
-        training_pairs_labels = np.load(saved_training_pairs_labels)
-
-    print("Starting training...")
     training_start_time = datetime.now().isoformat()
 
-    # model = SDML(verbose=True, preprocessor=landmarks_matrix, prior="identity", balance_param=0.4) #, convergence_threshold=1e-5)
-    # model.fit(training_pairs_indices, training_pairs_labels)
-    model = ITML(preprocessor=landmarks_matrix, max_iter=10_000, verbose=True)
-    model.fit(training_pairs_indices, training_pairs_labels)
+    if NAME_MODEL_TO_TRAIN == "ITML":
+        landmarks_matrix, training_pairs_indices, training_pairs_labels = generate_weakly_supervised_interpolated_dataset(
+            Path("dataset_metric_learning").resolve(), rates_list)
+        model = ITML(preprocessor=landmarks_matrix, max_iter=10_000, verbose=True)
+        model.fit(training_pairs_indices, training_pairs_labels)
+        MODELS_PATH.mkdir(exist_ok=True)
+        dump(model, str(MODELS_PATH.joinpath(f'LMNN_{training_start_time}.joblib')))
 
-    MODELS_PATH.mkdir(exist_ok=True)
+    elif NAME_MODEL_TO_TRAIN == "SDML":
+        landmarks_matrix, training_pairs_indices, training_pairs_labels = generate_weakly_supervised_interpolated_dataset(
+            Path("dataset_metric_learning").resolve(), rates_list)
+        model = SDML(preprocessor=landmarks_matrix, verbose=True)
+        model.fit(training_pairs_indices, training_pairs_labels)
+        MODELS_PATH.mkdir(exist_ok=True)
+        dump(model, str(MODELS_PATH.joinpath(f'SDML_{training_start_time}.joblib')))
 
-    dump(model, str(MODELS_PATH.joinpath(f'model_ITML_{training_start_time}.joblib')))
+    elif NAME_MODEL_TO_TRAIN == "MMC":
+        landmarks_matrix, training_pairs_indices, training_pairs_labels = generate_weakly_supervised_interpolated_dataset(
+            Path("dataset_metric_learning").resolve(), rates_list)
+        model = MMC(preprocessor=landmarks_matrix, verbose=True)
+        model.fit(training_pairs_indices, training_pairs_labels)
+        MODELS_PATH.mkdir(exist_ok=True)
+        dump(model, str(MODELS_PATH.joinpath(f'MMC_{training_start_time}.joblib')))
 
+    elif NAME_MODEL_TO_TRAIN == "LMNN":
+        landmarks_matrix, training_labels = generate_training_supervised_dataset_categorical(
+            Path("dataset_metric_learning").resolve())
+        model = LMNN(verbose=True)
+        model.fit(landmarks_matrix, training_labels)
+        MODELS_PATH.mkdir(exist_ok=True)
+        dump(model, str(MODELS_PATH.joinpath(f'LMNN_{training_start_time}.joblib')))
+
+    elif NAME_MODEL_TO_TRAIN == "NCA":
+        landmarks_matrix, training_labels = generate_training_supervised_dataset_categorical(
+            Path("dataset_metric_learning").resolve())
+        model = NCA(verbose=True)
+        model.fit(landmarks_matrix, training_labels)
+        MODELS_PATH.mkdir(exist_ok=True)
+        dump(model, str(MODELS_PATH.joinpath(f'NCA_{training_start_time}.joblib')))
+
+    elif NAME_MODEL_TO_TRAIN == "LFDA":
+        landmarks_matrix, training_labels = generate_training_supervised_dataset_categorical(
+            Path("dataset_metric_learning").resolve())
+        model = LFDA()
+        model.fit(landmarks_matrix, training_labels)
+        MODELS_PATH.mkdir(exist_ok=True)
+        dump(model, str(MODELS_PATH.joinpath(f'LFDA_{training_start_time}.joblib')))
 
 if __name__ == '__main__':
     main()
